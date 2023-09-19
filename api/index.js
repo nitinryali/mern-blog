40c5456baec109392d0055fea3c2fb88b3eac456
nitinryali <85013482+nitinryali@users.noreply.github.com>
2023-09-19 21:27:22 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ const fs = require("fs");
 const salt = bcrypt.genSaltSync(10);
 dotenv.config();
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "https://mern-blog-f9q0.onrender.com" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
