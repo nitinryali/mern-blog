00b734bd4c7420fa2db7185a6fbeafd6986b0c0b
nitinryali <85013482+nitinryali@users.noreply.github.com>
2023-09-19 11:16:06 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -148,7 +148,7 @@ app.listen(PORT);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/clinet/build")));
+  app.use(express.static(path.join(__dirname1, "/client/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
