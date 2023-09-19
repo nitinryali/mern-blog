60109d5482dc2a846bb47bb6fc53bfa69098e701
nitinryali <85013482+nitinryali@users.noreply.github.com>
2023-09-19 21:28:30 +0530

    Update EditPost.js

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -31,7 +31,7 @@ export default function EditPost() {
     if (files?.[0]) {
       data.set('file', files?.[0]);
     }
-    const response = await fetch('http://localhost:4000/post', {
+    const response = await fetch('https://mern-blog-f9q0.onrender.com/post', {
       method: 'PUT',
       body: data,
       credentials: 'include',
