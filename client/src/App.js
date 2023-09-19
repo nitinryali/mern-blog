c32180306dbce9dc1f07f5a74349ebdd0fb37b14
nitinryali <85013482+nitinryali@users.noreply.github.com>
2023-09-19 10:57:06 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import Layout from "./Layout";
+import { Route, Routes } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import PostPage from "./pages/PostPage";
+import { UserContextProvider } from "./UserContext";
+import CreatePost from "./pages/CreatePost";
+import EditPost from "./pages/EditPost";
+// import Layout from "./Layout";
+// import IndexPage from "./pages/IndexPage";
+// import LoginPage from "./pages/LoginPage";
+// import RegisterPage from "./pages/RegisterPage";
+// import { UserContextProvider } from './UserContext';
+
+function App() {
+  return (
+    <UserContextProvider>
+    <Routes>
+    <Route path="/" element={<Layout/>}>
+      <Route index element={<IndexPage />} />
+      <Route path={"/login"} element={<LoginPage/>} />
+      <Route path={"/register"} element={<RegisterPage/>} />
+      <Route path={"/Create"} element={<CreatePost/>} />
+      <Route path={"/post/:id"} element={<PostPage/>} />
+      <Route path={"/edit/:id"} element={<EditPost />} />
+
+
+      </Route>
+    </Routes>
+    </UserContextProvider>
+  );
+}
+
+export default App;
