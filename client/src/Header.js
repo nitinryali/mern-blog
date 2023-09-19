4c5df460ded63efa9edc012451067dfb63b43044
nitinryali <85013482+nitinryali@users.noreply.github.com>
2023-09-19 21:30:44 +0530

    Update Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -4,7 +4,7 @@ import { UserContext } from "./UserContext";
 export default function Header() {
   const {setUserInfo,userInfo} = useContext(UserContext);
   useEffect(()=>{
-    fetch('http://localhost:4000/profile',{
+    fetch('https://mern-blog-f9q0.onrender.com/profile',{
       credentials:'include',
     }).then(response =>{
       response.json().then(userInfo=>{
@@ -14,7 +14,7 @@ export default function Header() {
   },[]);
 
   function logout(){
-    fetch("http:localhost:4000/logout",{
+    fetch("https://mern-blog-f9q0.onrender.com/logout",{
       credentials:"include",
       method:"POST",
     });
